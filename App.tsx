03a290a2929da24fe0e27bd36103fa5904a54698
Leonardo Santos de Melo <leosantosmelo07@gmail.com>
2023-05-13 16:33:58 -0300

    fix: evoluindo o componente de mapview

--- App.tsx
+++ App.tsx
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-import MapView from 'react-native-maps';
-import {View, Dimensions, StyleSheet} from 'react-native';
+import {
+  View,
+  Dimensions,
+  StyleSheet,
+  PermissionsAndroid,
+  Platform,
+} from 'react-native';
+
+import MapView, {Region} from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 
 const {width, height} = Dimensions.get('screen');
 
 function App(): JSX.Element {
+  const [region, setRegion] = useState<Region>();
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(info => {
+      setRegion({
+        latitude: info.coords.latitude,
+        longitude: info.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+    });
+  });
+
   return (
     <View style={styles.container}>
       <MapView
-        style={{width: width, height: height}}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
+        onMapReady={() => {
+          Platform.OS === 'android'
+            ? PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+              )
+                .then(() => console.log('Usuário aceitou'))
+                .catch(() => console.log('Usuário negou'))
+            : '';
         }}
+        style={{width: width, height: height}}
+        region={region}
+        zoomEnabled={true}
+        minZoomLevel={17}
+        showsUserLocation={true}
+        loadingEnabled={true}
       />
     </View>
   );
